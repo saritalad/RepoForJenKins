dd1e86e80b33308016943fdd4cd60854c39df76f
saritalad <sarita.lad@rediffmail.com>
2022-05-16 15:31:57 +0530

    console project

---
+++ myconsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myconsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
